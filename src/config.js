854558e2d3244ed9ff0cbb05117bc921cd3ea245
Muhammad Huzaifa <mhuzaifa@nisum.com>
2023-04-05 16:16:13 +0500

    Initialize project using Create React App

---
+++ src/config.js
@@ -0,0 +1,139 @@
+export const carRegistryAbi = [
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "",
+        type: "string",
+      },
+    ],
+    name: "Cars",
+    outputs: [
+      {
+        internalType: "address",
+        name: "owner",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "number",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "model",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "manufacturer",
+        type: "string",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "number",
+        type: "string",
+      },
+    ],
+    name: "get",
+    outputs: [
+      {
+        components: [
+          {
+            internalType: "address",
+            name: "owner",
+            type: "address",
+          },
+          {
+            internalType: "string",
+            name: "number",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "model",
+            type: "string",
+          },
+          {
+            internalType: "string",
+            name: "manufacturer",
+            type: "string",
+          },
+        ],
+        internalType: "struct CarRegistry.Car",
+        name: "",
+        type: "tuple",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "number",
+        type: "string",
+      },
+    ],
+    name: "getOwner",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "string",
+        name: "number",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "model",
+        type: "string",
+      },
+      {
+        internalType: "string",
+        name: "manufacturer",
+        type: "string",
+      },
+    ],
+    name: "set",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "newOwner",
+        type: "address",
+      },
+      {
+        internalType: "string",
+        name: "number",
+        type: "string",
+      },
+    ],
+    name: "transfer",
+    outputs: [],
+    stateMutability: "payable",
+    type: "function",
+  },
+];
+
+export const contractAddress = "0x692Db73138f1279Dd5ee67FbBfe1685b2734809f";
